03cb1842a0448591e4afd388d50cd1b714eeb700
Javier Pulido <javier.pulido@sngular.team>
2020-06-08 21:59:50 +0200

    Subida de fichero terminada

--- src/es/unex/cum/iiisa/servlets/FileManagement.java
+++ src/es/unex/cum/iiisa/servlets/FileManagement.java
@@ -1,8 +1,5 @@
 package es.unex.cum.iiisa.servlets;
 
-import es.unex.cum.iiisa.io.Entrada;
-import es.unex.cum.iiisa.pathfinder.PathFinder;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -10,45 +7,73 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
-@WebServlet(name = "FileManagement")
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10,    // 10 MB
-        maxFileSize = 1024 * 1024 * 50,          // 50 MB
-        maxRequestSize = 1024 * 1024 * 100,      // 100 MB
-        location = "")
+@WebServlet(name = "/FileManagement")
+@MultipartConfig
 public class FileManagement extends HttpServlet {
 
-    private static final String SAVE_DIR = "uploads";
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        // gets absolute path of the web application
-        String appPath = System.getProperty("user.dir");
-        // constructs path of the directory to save uploaded file
-        String savePath = appPath + File.separator + SAVE_DIR;
-        File fileSaveDir = new File(savePath);
-        if (!fileSaveDir.exists()) {
-            fileSaveDir.mkdir();
-        }
-        System.out.println(fileSaveDir);
-        try {
-            //System.out.println(request.getParts());
-            for (Part part : request.getParts()) {
-                String fileName = extractFileName(part);
-                System.out.println(savePath + File.separator + fileName);
-                part.write(savePath + File.separator + fileName);
+        String name = request.getParameter("action");
+        System.out.println(name);
+        if (name.equals("fichero")) {
+            try {
+                leerFichero(request);
+            } catch (ServletException | IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException | ServletException e) {
-            e.printStackTrace();
+        } else if (name.equals("teclado")) {
+            leerTeclado(request);
+        }
+    }
+
+    private void leerTeclado(HttpServletRequest request) {
+        int r = Integer.parseInt(request.getParameter("r"));
+        int q = Integer.parseInt(request.getParameter("q"));
+        String[] nodos = request.getParameter("nodos").split(",");
+        String[] pares = request.getParameter("pares").split("\n");
+        System.out.println("Teclado!");
+        System.out.println("" + r);
+        System.out.println("" + q);
+        System.out.println("" + nodos.length);
+    }
+
+    private void leerFichero(HttpServletRequest request)
+            throws ServletException, IOException {
+        final Part filePart = request.getPart("file");
+        final String fileName = getFileName(filePart);
+        OutputStream out = null;
+        InputStream filecontent = null;
+        String path = request.getServletContext().getRealPath("") + File.separator
+                + fileName;
+        try {
+            System.out.println(path);
+            out = new FileOutputStream(new File(path));
+            filecontent = filePart.getInputStream();
+
+            int read;
+            final byte[] bytes = new byte[1024];
+
+            while ((read = filecontent.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+        } catch (FileNotFoundException ignore) {
+
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (filecontent != null) {
+                filecontent.close();
+            }
         }
         //TODO al terminar, llamar a new Entrada con el path del fichero en el servidor
-        Entrada e = new Entrada(new File(savePath+ "aqui el nombre del fichero generado"));
-        try {
-            e.procesarFichero();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
+        //Entrada e = new Entrada(new File(savePath + "aqui el nombre del fichero generado"));
+        //try {
+        //    e.procesarFichero();
+        //} catch (IOException ioException) {
+        //    ioException.printStackTrace();
+        //}
 
         // TODO nombres1 tendría que venir en el fichero
         // Llamada al algoritmo con los valores r y q que entran por teclado, al igual que los nombres
@@ -56,22 +81,19 @@ public class FileManagement extends HttpServlet {
         // TODO el metodo execute deberia devolver un listado de files que se guardan en la carpeta downloads en el server
         // Este método directamente genera los dos archivos de salida en la raíz del proyecto. Son los que habría que descargar
         //pfnet.execute();
-
-        System.out.println("Fichero guardado!");
     }
 
-    private String extractFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        String[] items = contentDisp.split(";");
-        for (String s : items) {
-            if (s.trim().startsWith("filename")) {
-                return s.substring(s.indexOf("=") + 2, s.length() - 1);
+    private String getFileName(final Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
             }
         }
-        return "";
+        return null;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("GET BIEEEEN");
     }
 }
